5add199610ba493da6ae73895ea2e2bc09a16871
rahbu <113043422+rahbu@users.noreply.github.com>
2023-01-06 10:00:47 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Interview-5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-qtgu3k)
---
+++ index.ts
@@ -0,0 +1,4 @@
+import './style.css';
+
+// 1. Implement a flat function (https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat)
+// 2. Implement a flat rxjs operator
